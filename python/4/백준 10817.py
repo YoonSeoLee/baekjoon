b85eb4cdd4265eff21d30951324276eba4cb4586
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2017-12-22 22:56:03 +0900

    if

--- python/4/백준 10817.py
+++ python/4/백준 10817.py
@@ -4,6 +4,18 @@ A = int(A)
 B = int(B)
 C = int(C)
 
-nList = [A, B, C]
-nList.sort()
-print(nList[1])
+# A > B > C
+if A <= B:
+    if B <= C:
+        print(B)
+    elif A <= C:
+        print(C)
+    else:
+        print(A)
+elif B <= A:
+    if A <= C:
+        print(A)
+    elif B <= C:
+        print(C)
+    else:
+        print(B)
