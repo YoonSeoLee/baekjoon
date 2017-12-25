ea48f78832f77257f466a83be313f6c448d029ee
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2017-12-25 19:45:48 +0900

    using map

--- python/2/백준 10998.py
+++ python/2/백준 10998.py
@@ -1,6 +1,18 @@
-A, B = input().split()
-
-A = int(A)
-B = int(B)
+A, B = map(int, input().split())
 
 print(A * B)
+
+
+# A > B > C
+if A <= B:
+    if B <= C:
+        A, C = C, A
+    elif A <= C:
+        A, B = B, A
+elif A <= C:
+    if B <= C:
+        print(A)
+
+
+
+print(B)
