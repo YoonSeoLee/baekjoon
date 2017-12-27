bf83c30fc92f644148f2308e530ddd8e94f00012
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2017-12-27 22:19:34 +0900

    1000

---
+++ java/1/baekjoon1000.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 2017-12-25.
+ */
+public class baekjoon1000
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int A, B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.println(A + B);
+    }
+}
