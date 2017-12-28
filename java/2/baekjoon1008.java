2da9ed78abfc8946e7977dd2b8bdacaf24c5297a
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2017-12-28 18:18:54 +0900

    1008 and 10998

---
+++ java/2/baekjoon1008.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 2017-12-26.
+ */
+public class baekjoon1008
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        float A, B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.println(A / B);
+    }
+}
---
+++ java/2/baekjoon10998.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 2017-12-26.
+ */
+public class baekjoon10998
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int A, B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.println(A * B);
+    }
+}
