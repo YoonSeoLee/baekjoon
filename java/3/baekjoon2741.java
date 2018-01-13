11f1e8e423345f998bead067fa40a0518e7b533f
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2018-01-13 22:47:16 +0900

    2741 and 2742

---
+++ java/3/baekjoon2741.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 2018-01-11.
+ */
+public class baekjoon2741
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for(int i = 1; i <= N; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ java/3/baekjoon2742.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 2018-01-11.
+ */
+public class baekjoon2742
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for(int i = N; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
