3bc958578024a0a65407de53ae7a1af5be882140
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2018-01-26 23:26:57 +0900

    1001

---
+++ c/2/백준1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+    int difference;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    difference = A - B;
+    printf("%d", difference);
+    
+    return 0;
+}
