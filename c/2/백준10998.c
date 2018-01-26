b545303b77616a170ad1d68a122bf4c8b55fb6e6
YoonSeoLee <33597492+YoonSeoLee@users.noreply.github.com>
2018-01-26 23:30:50 +0900

    10998

---
+++ c/2/백준10998.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+    int product;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    product = A * B;
+    printf("%d", product);
+    
+    return 0;
+}
