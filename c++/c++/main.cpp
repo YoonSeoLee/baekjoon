5541bfcc47abecf45355e6bea54899e26d09eb16
YoonSeoLee <westyun1997@gmail.com>
2020-03-08 03:30:37 +0900

    first try

---
+++ c++/c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by 이윤서 on 08/03/2020.
+//  Copyright © 2020 이윤서. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
